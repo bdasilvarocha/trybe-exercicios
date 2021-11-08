82a178b6b5b4323214dbe83f0b0d7d425385c69c
Bruno Rocha <bdasilvarocha@gmail.com>
2021-11-08 08:23:04 -0300

    4.1 Primeiro exercicio de javascript

---
+++ fundamentos/Bloco-4/dia-1/script.js
@@ -0,0 +1,9 @@
+const myName = "Bruno";
+const birthCity = "Brasilia";
+let birthYear = 1993;
+console.log("Meu nome eh " + myName + ", nasci em " + birthCity + " no ano de " + birthYear);
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Patos de Minas";
+console.log(birthCity);
