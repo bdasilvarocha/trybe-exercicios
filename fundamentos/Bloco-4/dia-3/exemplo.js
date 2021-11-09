782179fc608d3028833004f0726327b25d68680c
Bruno Rocha <bdasilvarocha@gmail.com>
2021-11-09 18:14:56 -0300

    Exercicio da dinamica da ao vivo

--- fundamentos/Bloco-4/dia-3/exemplo.js
+++ fundamentos/Bloco-4/dia-3/exemplo.js
@@ -1,11 +1,26 @@
-let fruits = [3, 4, 10, 1, 12];
-let soma = 0;
+// EXEMPLO FOR DENTRO DE FOR 
+let qtd = 10; 
+let ingredientes = ["queijo", "presunto", "maionese"]; 
+let sacola = []; 
 
-for (let i = 0; i < fruits.length; i += 1) {
-    soma += fruits[i];
-}
-if (soma > 15) {
-    console.log('O valor da soma eh ' + soma);
-}else {
-    console.log('Valor menor que 16')
-}
+for (let index = 0; index < 10; index += 1) { 
+    //console.log('exectuou fora: ', index) 
+    let pao = []; 
+        for (let index = 0; index < ingredientes.length; index += 1) { 
+        // console.log('exectuou dentro: ', index); 
+        pao.push(ingredientes[index]) 
+        } 
+    sacola.push(pao); 
+} 
+
+// console.log(sacola); 
+//let item = 1; 
+//let linha = 5; 
+//sacola[(linha - 1)].splice(item - 1, 1) 
+// linha.splice(linha - 1, 1) 
+//console.log(sacola); 
+
+let produto = "queijo";
+linha = 7; 
+sacola[(linha - 1)].splice(ingredientes.indexOf(produto), 1) 
+console.log(sacola); 
