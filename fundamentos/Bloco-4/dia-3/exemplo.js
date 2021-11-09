6c8884d686445c327ef565d743bf564e5a426b3a
Bruno Rocha <bdasilvarocha@gmail.com>
2021-11-09 15:50:19 -0300

    iniciando dia 3 em JS

---
+++ fundamentos/Bloco-4/dia-3/exemplo.js
@@ -0,0 +1,11 @@
+let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+for (let i = 0; i < fruits.length; i += 1) {
+    soma += fruits[i];
+}
+if (soma > 15) {
+    console.log('O valor da soma eh ' + soma);
+}else {
+    console.log('Valor menor que 16')
+}
