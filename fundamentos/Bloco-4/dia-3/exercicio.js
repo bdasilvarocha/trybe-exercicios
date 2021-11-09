e9b5417bc5950507520223ccc39b1b7c08bfd52c
Bruno Rocha <bdasilvarocha@gmail.com>
2021-11-09 20:07:54 -0300

    Completando exercicios 3

--- fundamentos/Bloco-4/dia-3/exercicio.js
+++ fundamentos/Bloco-4/dia-3/exercicio.js
@@ -2,7 +2,7 @@
 let n = 5;
 let a = "";
 
-if (n > 1){
+/*if (n > 1){
     for(i = 0; i < n; i += 1){
         let a = ""
         for (j =0; j < n; j += 1){
@@ -12,7 +12,7 @@ if (n > 1){
     }
 } else {
     console.log('N precisa ter valor n > 1');
-}
+}*/
 
 
 //2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base
@@ -24,3 +24,19 @@ if (n > 1){
 } else {
     console.log('N precisa ter valor n > 1');
 }*/
+
+//3- Agora inverta o lado do triângulo.
+if (n > 1){
+    for(i = 0; i < n; i += 1){
+        let a = "";
+        for (j = n - 1; j > i; j -= 1){
+            a = a + " ";
+        }
+        for (j = 0; j <= i; j += 1){
+            a = a + "*";
+        }
+        console.log(a);
+    }
+} else {
+    console.log('N precisa ter valor n > 1');
+}
