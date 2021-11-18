6a7b1a8e66f6804e7dc5e5ad9f06cd1edb680398
Bruno Rocha <bdasilvarocha@gmail.com>
2021-11-18 20:26:43 -0300

    2. adiciona butao Feriados

--- fundamentos/Bloco-5/dia-3/script.js
+++ fundamentos/Bloco-5/dia-3/script.js
@@ -1,5 +1,5 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
     const weekDaysList = document.querySelector('.week-days');
   
     for (let index = 0; index < weekDays.length; index += 1) {
@@ -9,26 +9,38 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  };
+};
   
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-  // 1.
-function diasDoCalendario(){
-    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-    const days = document.getElementById('days'); 
-    for (let i = 0; i < dezDaysList.length; i += 1) {
-        const listaDias = document.createElement('li');
-        listaDias.innerText = dezDaysList[i];
-        listaDias.className = 'days';
-        days.appendChild(listaDias);
-        if (dezDaysList[i] == 24 || dezDaysList[i] == 25 || dezDaysList[i] == 31){
+    createDaysOfTheWeek();
+    
+    // Escreva seu código abaixo.
+    // 1.
+    function diasDoCalendario() {
+        const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+        const days = document.getElementById('days'); 
+        for (let i = 0; i < dezDaysList.length; i += 1) {
+            const listaDias = document.createElement('li');
+            listaDias.innerText = dezDaysList[i];
+            listaDias.className = 'days';
+            days.appendChild(listaDias);
+            if (dezDaysList[i] == 24 || dezDaysList[i] == 25 || dezDaysList[i] == 31) {
             listaDias.classList.add('holiday');
-        }
-        if (dezDaysList[i] == 4 || dezDaysList[i] == 11 || dezDaysList[i] == 18 || dezDaysList[i] == 25) {
+            }
+            if (dezDaysList[i] == 4 || dezDaysList[i] == 11 || dezDaysList[i] == 18 || dezDaysList[i] == 25) {
             listaDias.classList.add('friday');
-        }
-    }
-}
-diasDoCalendario();
+            }
+        };
+    };   
+    diasDoCalendario();
+
+// 2.
+    const str = 'Feriados'
+    function btnFeriados(str) {
+        const butoes = document.getElementsByClassName('buttons-container')[0];
+        const feriadoButao = document.createElement('button');
+        feriadoButao.innerText = str;
+        feriadoButao.id = 'btn-holiday';
+        butoes.appendChild(feriadoButao);
+    };
+    btnFeriados(str);
+    
