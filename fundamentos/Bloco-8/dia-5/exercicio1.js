9fc4a1040c2da0b37fe32b42ff820aeeb4662827
Bruno Rocha <bdasilvarocha@gmail.com>
2022-01-11 15:20:08 -0300

    Finalizando

--- fundamentos/Bloco-8/dia-5/exercicio1.js
+++ fundamentos/Bloco-8/dia-5/exercicio1.js
@@ -45,3 +45,43 @@ const personLikes = ({name, age, likes}) => `${name} is ${age} years old and lik
 
 console.log(personLikes(alex));
 console.log(personLikes(gunnar));
+
+// Exercicio 04
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+// escreva filterPeople abaixo
+
+const {name, nationality} = people;
+const filterPeople = (nationality) => {
+  if (nationality === 'Australian') {
+    console.log(`${name} eh australiano`);
+  }
+};
+
+const filteredPeople = people.filter(filterPeople);
+console.log(filteredPeople)
