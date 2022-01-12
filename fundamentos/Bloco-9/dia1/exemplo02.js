14300a02a5cda6a624f6c25dfbf1d29feffe2d03
Bruno Rocha <bdasilvarocha@gmail.com>
2022-01-12 13:23:37 -0300

    exercicios do dia 1 - bloco 9

---
+++ fundamentos/Bloco-9/dia1/exemplo02.js
@@ -0,0 +1,39 @@
+// // Definição da função userFullName
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// // Definição da função getUser
+// const getUser = (param) => {
+//   const userToReturn = {
+//     firstName: "Ivan",
+//     lastName: "Ivanovich",
+//     nationality: "Russian"
+//   };
+//   // Retornamos nosso parâmetro, que será uma função (callback)
+//   return param(userToReturn);
+// };
+
+// // Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+// console.log(getUser(userFullName));
+// console.log(getUser(userNationality));
+
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (param) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    console.log(user);
+  }, delay());
+};
+
+console.log(getUser(userFullName)); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
